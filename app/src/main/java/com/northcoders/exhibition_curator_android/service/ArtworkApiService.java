744c463575013bdd264914ad5322924c4b7855e2
qiuqiu <qiujuanwang87@gmail.com>
2025-03-07 23:30:54 +0000

    fix: correct the json response from Map to Artwork in ArtworkApiService

--- app/src/main/java/com/northcoders/exhibition_curator_android/service/ArtworkApiService.java
+++ app/src/main/java/com/northcoders/exhibition_curator_android/service/ArtworkApiService.java
@@ -1,6 +1,8 @@
 package com.northcoders.exhibition_curator_android.service;
 
 import com.northcoders.exhibition_curator_android.model.Artwork;
+import com.northcoders.exhibition_curator_android.model.ArtworkResponse;
+
 import java.util.Map;
 import retrofit2.Call;
 import retrofit2.http.Body;
@@ -14,7 +16,7 @@ import retrofit2.http.Query;
 public interface ArtworkApiService {
 
     @GET("search-results/{museum}")
-    Call<Map<String, Object>> searchArtworks(
+    Call<ArtworkResponse> searchArtworks(
             @Path("museum") String museum,
             @Query("keyword") String keyword,
             @Query("artist") String artist,
@@ -27,6 +29,5 @@ public interface ArtworkApiService {
             @Path("museum") String museum,
             @Path("sourceId") String sourceId
     );
-
 
 }
